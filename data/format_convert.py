1e1dce9bf134030b646577739f02b79e2e1f3bf4
WK <kyou.wk@gmail.com>
2017-03-11 20:20:41 +0800

    new vertex format

--- data/format_convert.py
+++ data/format_convert.py
@@ -5,8 +5,8 @@ import sys
 
 #src_vertex src_vertex_val vertex_2 edge_2 vertex_3 edge_3
 def format_giraph(unf_net):
-    edge_val = 0
-    vertex_val = 0
+    edge_val = 1
+    vertex_val = 1
     out_line = ""
     for k in unf_net.keys():
         edges = unf_net[k]
@@ -14,6 +14,26 @@ def format_giraph(unf_net):
         for edge in edges:
             out_line = out_line + " " + str(edge) + " " + str(edge_val)
         out_line = out_line + "\n"
+    return(out_line)
+
+#JSONArray(<vertex id>, <vertex value>, JSONArray(JSONArray(<dest vertex id>, <edge value>), ...))
+# Here is an example with vertex id 1, vertex value 4.3, and two edges.
+# First edge has a destination vertex 2, edge value 2.1.
+# Second edge has a destination vertex 3, edge value 0.7.
+#     [1,4.3,[[2,2.1],[3,0.7]]]
+def format_jsonldfdvertex(unf_net):
+    edge_val = 1
+    vertex_val = 1
+    out_line = ""
+    for k in unf_net.keys():
+        edges = unf_net[k]
+        last_edge = edges[len(edges)-1]
+        out_line = out_line + "[" + str(k) + "," + str(vertex_val) + ",["
+        for edge in edges:
+            out_line = out_line + "[" + str(edge) + "," + str(edge_val) + "]"
+            if edge is not last_edge:
+                out_line = out_line + ","
+        out_line = out_line + "]]\n"
     return(out_line)
 
 
@@ -57,10 +77,13 @@ def main(argv):
 
     f1_net = format_giraph(unf_net)
     f2_net = format_glab(unf_net)
+    f3_net = format_jsonldfdvertex(unf_net)
     print("Writing Giraph format.")
     write_net(f1_net, "erdos_giraph.net")
     print("Writing Graphlab format.")
     write_net(f2_net, "erdos_graphlab.net")
+    print("Writing Giraph JSONLongDoubleFloatDoubleVertex format.")
+    write_net(f3_net, "erdos_giraph_json.net")
     
 if __name__ == '__main__':
     main(sys.argv)
