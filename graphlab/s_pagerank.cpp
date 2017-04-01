f924d297a3882bd665055a48be7427d6d481a66f
WK <kyou.wk@gmail.com>
2017-04-01 11:10:09 +0800

    updates

--- graphlab/s_pagerank.cpp
+++ graphlab/s_pagerank.cpp
@@ -6,10 +6,10 @@
 
 float RESET_PROB = 0.15;
 float TOLERANCE = 1.0E-2;
-// vertex data is pagerank value
-typedef float vertex_data_type;
 // no edge data
 typedef graphlab::empty edge_data_type;
+// vertex data is pagerank value
+typedef float vertex_data_type;
 // graph type will be determined by the vertex and edge data types
 typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type;
 // to init vertices
@@ -22,30 +22,23 @@ class pagerank :
   float last_change;
 public:
   /* Gather weighted rank of the adjacent page   */
-  float gather(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
-    return ((1.0 - RESET_PROB) / edge.source().num_out_edges()) *
-      edge.source().data();
+  float gather(icontext_type& context, const vertex_type& vertex, edge_type& edge) const {
+    return ((1.0 - RESET_PROB) / edge.source().num_out_edges()) * edge.source().data();
   }
-
   /* Update vertex rank with sum of adjacent vertices rank */
-  void apply(icontext_type& context, vertex_type& vertex,
-             const gather_type& total) {
+  void apply(icontext_type& context, vertex_type& vertex, const gather_type& total) {
     const double newval = total + RESET_PROB;
     last_change = std::fabs(newval - vertex.data());
     vertex.data() = newval;
   }
 
   /* The scatter edges depend on whether the pagerank has converged */
-  edge_dir_type scatter_edges(icontext_type& context,
-                              const vertex_type& vertex) const {
+  edge_dir_type scatter_edges(icontext_type& context, const vertex_type& vertex) const {
     if (last_change > TOLERANCE) return graphlab::OUT_EDGES;
     else return graphlab::NO_EDGES;
   }
-
   //signal adjacent vertices
-  void scatter(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
+  void scatter(icontext_type& context, const vertex_type& vertex, edge_type& edge) const {
     context.signal(edge.target());
   }
 }; // pagerank calc
@@ -74,18 +67,13 @@ int main(int argc, char** argv) {
   std::string graph_dir;
   std::string format = "adj"; //default graph format adjacency list
   std::string exec_type = "synchronous"; //synchronous execution
-  clopts.attach_option("graph", graph_dir,
-                       "The graph file");
+  clopts.attach_option("graph", graph_dir, "The graph file");
   clopts.add_positional("graph");
-  clopts.attach_option("format", format,
-                       "graph file format");
-  clopts.attach_option("engine", exec_type, 
-                       "Synchronous or asynchronous");
-  clopts.attach_option("tol", TOLERANCE,
-                       "The permissible change at convergence.");
+  clopts.attach_option("format", format, "graph file format");
+  clopts.attach_option("engine", exec_type, "Synchronous or asynchronous");
+  clopts.attach_option("tol", TOLERANCE, "The permissible change at convergence.");
   std::string saveprefix;
-  clopts.attach_option("saveprefix", saveprefix,
-                       "prefix of outfile");
+  clopts.attach_option("saveprefix", saveprefix, "prefix of outfile");
 
   if(!clopts.parse(argc, argv)) {
     dc.cout() << "Error in parsing command line arguments." << std::endl;
@@ -103,8 +91,7 @@ int main(int argc, char** argv) {
   graph.load_format(graph_dir, format);
   // must call finalize before querying the graph
   graph.finalize();
-  dc.cout() << "#vertices: " << graph.num_vertices()
-            << " #edges:" << graph.num_edges() << std::endl;
+  dc.cout() << "#vertices: " << graph.num_vertices() << " #edges:" << graph.num_edges() << std::endl;
 
   // Init vertex data
   graph.transform_vertices(init_vertex);
@@ -114,8 +101,7 @@ int main(int argc, char** argv) {
   engine.signal_all();
   engine.start();
   const float runtime = engine.elapsed_seconds();
-  dc.cout() << "Finished Running engine in " << runtime
-            << " seconds." << std::endl;
+  dc.cout() << "Finished Running engine in " << runtime << " seconds." << std::endl;
 
   //save graph
   if (saveprefix != "") {
